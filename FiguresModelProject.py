783600410eaf011ea268941d9366748a58ffeb36
ThorKranold <carlsenthor@gmail.com>
2026-08-21 22:27:52 +0200

    Add files via upload

---
+++ FiguresModelProject.py
@@ -0,0 +1,93 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+def plot_grid(opt,title=''):
+    """ utility over the nested shares, as a surface and as a contour plot
+
+    Args:
+
+        opt (SimpleNamespace): a solution from ConsumerClass.solve_grid()
+        title (str): name of the calibration, put in the panel titles
+
+    """
+
+    fig = plt.figure(figsize=(14,5.5))
+
+    # the surface
+    ax = fig.add_subplot(1,2,1,projection='3d')
+
+    ax.plot_surface(opt.s1_grid,opt.w_grid,opt.u_grid,cmap='viridis')
+    ax.plot([opt.s1],[opt.w],[opt.u],'o',ms=9,color=colors[3],label='grid search')
+
+    ax.set_title(f'Utility({title})')
+    ax.set_xlabel('$s_1$')
+    ax.set_ylabel('$w$')
+    ax.set_zlabel('utility, $u$')
+    ax.legend(loc='upper left',fontsize=11)
+
+    # the contour plot
+    ax = fig.add_subplot(1,2,2)
+
+    cs = ax.contourf(opt.s1_grid,opt.w_grid,opt.u_grid,levels=30)
+    fig.colorbar(cs,ax=ax,label='utility, $u$')
+    ax.plot(opt.s1,opt.w,'o',ms=9,color=colors[3],label='grid search')
+
+    ax.set_title(f'Contours ({title})')
+    ax.set_xlabel('$s_1$')
+    ax.set_ylabel('$w$')
+    ax.set_xlim(0,1)
+    ax.set_ylim(0,1)
+    ax.legend(loc='upper right',fontsize=11)
+    ax.grid(visible=False)
+
+    fig.tight_layout()
+    plt.show()
+
+
+def plot_convergencepath(opt_grid,opt,title=''):
+    """ the convergence path on the contour plot, and the distance to the end point
+
+    Args:
+
+        opt_grid (SimpleNamespace): a solution from ConsumerClass.solve_grid()
+        opt (SimpleNamespace): a solution from ConsumerClass.solve()
+        title (str): name of the calibration, put in the panel titles
+
+    """
+
+    fig = plt.figure(figsize=(14,5.5))
+
+    # a. the path on top of the contours
+    ax = fig.add_subplot(1,2,1)
+
+    cs = ax.contourf(opt_grid.s1_grid,opt_grid.w_grid,opt_grid.u_grid,levels=30)
+    fig.colorbar(cs,ax=ax,label='utility, $u$')
+
+    ax.plot(opt.path[:,0],opt.path[:,1],'-o',ms=5,color=colors[3],label='path')
+    ax.plot(opt.path[0,0],opt.path[0,1],'s',ms=9,color=colors[1],label='start')
+    ax.plot(opt.s1,opt.w,'*',ms=15,color=colors[2],label='end')
+
+    ax.set_title(f'Convergence path ({title})')
+    ax.set_xlabel('$s_1$')
+    ax.set_ylabel('$w$')
+    ax.set_xlim(0,1)
+    ax.set_ylim(0,1)
+    ax.legend(loc='upper right',fontsize=11)
+    ax.grid(visible=False)
+
+    # b. distance to the end point
+    ax = fig.add_subplot(1,2,2)
+
+    dist = np.sqrt(np.sum((opt.path-opt.path[-1])**2,axis=1))
+
+    ax.plot(np.arange(dist.size-1),dist[:-1],'-o',ms=5,color=colors[3])
+
+    ax.set_yscale('log')
+    ax.set_title(f'Distance to the end point ({title})')
+    ax.set_xlabel('iteration')
+    ax.set_ylabel('$\\|s_k - s_{end}\\|$')
+
+    fig.tight_layout()
+    plt.show()
